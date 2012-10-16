e1034fcb6ed63d1e107425b0630aed7575d6c0fd
Elia Schito <elia@schito.me>
2012-10-16 10:50:59 +0200

    Fail fast if opal.coffee doesn't compile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 RSpec.configure do |c|
-  c.before :suite do
+  c.before :all do
+    compile_opal_coffee.should be_true
+  end
+
+  def compile_opal_coffee
     system "coffee -c #{File.expand_path('../../lib/opal.coffee', __FILE__)}"
   end
 end

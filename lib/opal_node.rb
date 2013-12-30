634696235a5f9dc5060a891a01a45719674493b7
Elia Schito <elia@schito.me>
2013-12-30 10:30:03 +0100

    Fix some stdout stuff (should be backported to opal)

--- lib/opal_node.rb
+++ lib/opal_node.rb
@@ -1,5 +1,29 @@
+module IO::Writable
+  def puts(*args)
+    write args.map { |arg| String(arg) }.join($/)+$/
+  end
+end
+
+$stdout = IO.new
+$stderr = IO.new
+STDOUT = $stdout
+STDERR = $stderr
+
+def $stdout.write(string)
+  `process.stdout.write(#{string})`
+  nil
+end
+
+def $stderr.write(string)
+  `process.stderr.write(string)`
+  nil
+end
+
+$stdout.extend(IO::Writable)
+$stderr.extend(IO::Writable)
+
 module Kernel
-  def print *args
-    `process.stdout.write(#{args.join})`
+  def require name
+    `require(#{name})`
   end
 end

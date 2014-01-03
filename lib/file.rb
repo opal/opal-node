7e81a3e995eda2c64169b9b361fcf220bfbccf20
Elia Schito <elia@schito.me>
2014-01-03 12:59:19 +0100

    Add File.exist?

--- lib/file.rb
+++ lib/file.rb
@@ -3,6 +3,10 @@ class File
     `#{__fs__}.readFileSync(#{path}).toString()`
   end
 
+  def self.exist? path
+    `#{__fs__}.existsSync(#{path})`
+  end
+
   def self.__fs__
     @fs ||= `OpalNode.node_require('fs')`
   end
